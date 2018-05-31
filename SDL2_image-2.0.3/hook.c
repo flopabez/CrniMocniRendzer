749bb8eb4c08b4121c8b9c8d56c98b206e103551
flopabez <33830264+flopabez@users.noreply.github.com>
2018-06-01 01:23:41 +0200

    Add files via upload

---
+++ SDL2_image-2.0.3/hook.c
@@ -0,0 +1,83 @@
+int processEvents(SDL_Window *window, struct movementWrapper *wrap) {
+
+  SDL_Event event;
+  int done = 0;
+
+  while(SDL_PollEvent(&event)){
+
+    switch(event.type){
+      case SDL_WINDOWEVENT_CLOSE:
+          if(window){
+              SDL_DestroyWindow(window);
+              window = NULL;
+              done = 1;
+          }
+          break;
+      case SDL_KEYDOWN:
+        switch(event.key.keysym.sym){
+          case SDLK_ESCAPE:
+            done = 1;
+            break;
+          case SDLK_w:
+          case SDLK_UP:
+            wrap->up = 1;
+            wrap->tenkic->direction = 0;
+            break;
+          case SDLK_a:
+          case SDLK_LEFT:
+            wrap->left = 1;
+            wrap->tenkic->direction = 1;
+            break;
+          case SDLK_s:
+          case SDLK_DOWN:
+            wrap->down = 1;
+            wrap->tenkic->direction = 2;
+            break;
+          case SDLK_d:
+          case SDLK_RIGHT:
+            wrap->right = 1;
+            wrap->tenkic->direction = 3;
+            break;
+        }
+        break;
+      case SDL_KEYUP:
+        switch(event.key.keysym.sym){
+          case SDLK_w:
+          case SDLK_UP:
+            wrap->up = 0;
+            if(wrap->left) wrap->tenkic->direction = 1;
+            if(wrap->down) wrap->tenkic->direction = 2;
+            if(wrap->right) wrap->tenkic->direction = 3;
+            break;
+          case SDLK_a:
+          case SDLK_LEFT:
+            wrap->left = 0;
+            if(wrap->up) wrap->tenkic->direction = 0;
+            if(wrap->down) wrap->tenkic->direction = 2;
+            if(wrap->right) wrap->tenkic->direction = 3;
+            break;
+          case SDLK_s:
+          case SDLK_DOWN:
+            wrap->down = 0;
+            if(wrap->left) wrap->tenkic->direction = 1;
+            if(wrap->up) wrap->tenkic->direction = 0;
+            if(wrap->right) wrap->tenkic->direction = 3;
+            break;
+          case SDLK_d:
+          case SDLK_RIGHT:
+            wrap->right = 0;
+            if(wrap->left) wrap->tenkic->direction = 1;
+            if(wrap->down) wrap->tenkic->direction = 2;
+            if(wrap->up) wrap->tenkic->direction = 0;
+            break;
+        }
+        break;
+      case SDL_QUIT:
+        done = 1;
+        break;
+    }
+
+  }
+
+  return done;
+}
---
+++ SDL2_image-2.0.3/strukture.h
@@ -0,0 +1,19 @@
+#ifndef STRUKURE_H
+#define STRUKURE_H
+
+struct listNode;
+struct listNode* newNode(void* data);
+void insertBefore(struct listNode** stack, void* data);
+void removeNode(struct listNode** stack);
+
+struct Tank;
+struct movementWrapper;
+struct Bullet;
+struct gameState;
+struct Level;
+char squareCollision(int Ax, int Ay, int Awidth, int Bx, int By, int Bwidth);
+void Move(struct gameState* state, struct Tank* tenkic, char direction);
+void updateBullets(struct gameState* state, struct listNode* bulletlist);
+void fireBullet(struct gameState* state, struct Tank* tenkic);
+
+#endif
