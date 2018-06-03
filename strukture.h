0021954bdb561e2cc466891509ee7b7f2c9a909c
flopabez <33830264+flopabez@users.noreply.github.com>
2018-06-03 19:17:05 +0200

    Update strukture.h

--- strukture.h
+++ strukture.h
@@ -1,16 +1,52 @@
-#ifndef STRUKURE_H
-#define STRUKURE_H
+#ifndef STRUKTURE_H
+#define STRUKTURE_H
 
 
-struct listNode;
+struct listNode {
+	void* data;
+	struct listNode* next;
+};
+
+struct Tank {
+	int xPos, yPos, width;
+	char direction, speed;
+	char upgrade, lives, hitPoints, bulletSpeed, bulletPower, inAir, score;
+	char team, bot, frame;
+};
+
+struct movementWrapper {
+	char up, left, down, right;
+	struct Tank* tenkic;
+};
+
+struct Bullet {
+	struct Tank* source;
+	int xPos, yPos, width;
+	char direction, speed, power;
+};
+
+struct gameState {
+	struct listNode *playerTanks, *enemyTanks;
+	struct listNode *playerBullets, *enemyBullets;
+	char **terrain;
+	char height, width;
+	char timeStop; //clock pickup timer
+
+	void* renderer;
+	void* sprites;
+	int time;
+
+};
+
+struct Level {
+	struct gameState* state;
+	struct listNode* enemies;
+};
+
+
 struct listNode* newNode(void* data);
 void insertBefore(struct listNode** stack, void* data);
 void removeNode(struct listNode** stack);
-struct Tank;
-struct movementWrapper;
-struct Bullet;
-struct gameState;
-struct Level;
 char squareCollision(int Ax, int Ay, int Awidth, int Bx, int By, int Bwidth);
 struct Tank* tankCollision(struct gameState* state, struct Tank* tenkic);
 void Move(struct gameState* state, struct Tank* tenkic, char direction);
@@ -18,6 +54,5 @@ void updateBullets(struct gameState* state, struct listNode* bulletlist);
 void fireBullet(struct gameState* state, struct Tank* tenkic);
 void respawn(struct Tank* tenkic);
 void hitDetection(struct gameState* state);
-
 
 #endif
