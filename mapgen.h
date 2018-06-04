ba34f89291994b9996316356b9a889564aabcee9
Tatomirovic <39652686+tatomirovic@users.noreply.github.com>
2018-06-04 17:28:01 +0200

    Update mapgen.h

--- mapgen.h
+++ mapgen.h
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include<SDL.h>
 
+void read_map(char** map, int* map_h, int* map_w);
 void generate_tiles(char** map, int map_h, int map_w, int amount, int type);
 void set_map_area(char** map, int x, int y, int map_h, int map_w, int type, int big);
 void create_base(char** map, int map_h, int map_w);
@@ -11,7 +12,7 @@ void reset_map(char** map, int map_h, int map_w);
 void print_map(char** map, int map_h, int map_w);
 void deallocate_map(char** map, int map_h);
 char** allocate_map(int map_h, int map_w);
-void render_map(SDL_Renderer* renderer, SDL_Texture* sprites, char ** map, int map_h, int map_w, int x_brush, int y_brush, int time);
+void render_map(SDL_Renderer* renderer, SDL_Texture* sprites, char ** map, int map_h, int map_w, int x_brush, int y_brush, int time,int big);
 void create_base(char** map, int map_h, int map_w);
 int build_map(int map_height, int map_width);
 int random(int max);
