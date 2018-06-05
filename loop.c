c4a1fc1138f930adbee47c00a564bf7bef0b5664
flopabez <33830264+flopabez@users.noreply.github.com>
2018-06-05 21:26:06 +0200

    Update loop.c

--- loop.c
+++ loop.c
@@ -3,71 +3,131 @@
 #include <stdio.h>
 #include "mapgen.h"
 #include "hook.h"
+#include <SDL.h>
+#include <SDL_image.h>
 
 #undef main
 #define BLOCK_X 48
-#define FPS 60
+
+
+void render_map2(SDL_Renderer* renderer, SDL_Texture* sprites, char ** map, int map_h, int map_w, struct Tank* player, int time, struct Bullet* metak)
+{
+	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
+	SDL_RenderClear(renderer);
+	SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+	for (int i = 0; i < map_h; i++) {
+		for (int j = 0; j < map_w; j++) {
+			if (!map[i][j]) continue;//nista za prazan blok
+			if (map[i][j] == 6)
+			{
+				if ((i == map_h - 4) && (map[i][j + 3] == 6))
+				{
+					SDL_Rect brect = { j*BLOCK_X / 4, i*BLOCK_X / 4, BLOCK_X, BLOCK_X };
+					SDL_Rect bdest = { 256, 80, 16, 16 };
+					SDL_RenderCopy(renderer, sprites, &bdest, &brect);//dodavanje brusha u renderer
+				}
+			}
+			else if (map[i][j] != FOREST)
+			{
+				SDL_Rect location = { j*BLOCK_X / 4, i*BLOCK_X / 4, BLOCK_X / 4, BLOCK_X / 4 };
+				SDL_Rect block = { 256 + ((j % 4) * 16 / 4) + (map[i][j] == 3)*((time % 90) / 15) * 16 , (map[i][j] - 1) * 16 + ((i % 4) * 16 / 4), 16 / 4, 16 / 4 };
+				SDL_RenderCopy(renderer, sprites, &block, &location);//dodavanje bloka na lokaciju u rendereru
+			}
+		}
+	}
+
+	SDL_Rect rect = { player->xPos, player->yPos, BLOCK_X, BLOCK_X };
+	SDL_Rect dest = { 144 + 32 * player->direction, 0, 16, 16 };
+	SDL_RenderCopy(renderer, sprites, &dest, &rect);//dodavanje tenka u renderer
+	if (player->inAir)
+	{
+		SDL_Rect rect = { metak->xPos*BLOCK_X / 4, metak->yPos*BLOCK_X / 4, BLOCK_X, BLOCK_X };
+		SDL_Rect dest = { 310 + 16 * metak->direction, 96, 16, 16 };
+		SDL_RenderCopy(renderer, sprites, &dest, &rect);//dodavanje tenka u renderer
+	}
+
+	for (int i = 0; i < map_h; i++) {
+		for (int j = 0; j < map_w; j++) {
+			if (map[i][j] == FOREST)
+			{
+				SDL_Rect location = { j*BLOCK_X / 4, i*BLOCK_X / 4, BLOCK_X / 4, BLOCK_X / 4 };
+				SDL_Rect block = { 256 + ((j % 4) * 16 / 4)  , (map[i][j] - 1) * 16 + ((i % 4) * 16 / 4), 16 / 4, 16 / 4 };
+				SDL_RenderCopy(renderer, sprites, &block, &location);//dodavanje bloka na lokaciju u rendereru
+
+			}
+		}
+	}
+}
 
 int main() {
-	
-	struct gameState *state = (struct gameState*)malloc(sizeof(struct gameState));
-	SDL_Window *window = NULL;                    // Declare a window
-	SDL_Renderer *renderer = NULL;                // Declare a renderer
 
-	SDL_Init(SDL_INIT_VIDEO);              // Initialize SDL2
 
-	(*state).playerTanks = 0;
-	struct Tank* player = (struct Tank*)malloc(sizeof(struct Tank));
-	insertBefore(&((*state).playerTanks), player);
-	(*state).width = 52;
-	(*state).height = 52;
-	(*state).terrain = (char**)malloc(state->height * sizeof(char*));
-	for (int i = 0; i < state->height; i++) state->terrain[i] = (char*)calloc(state->width,sizeof(char));
+
+	struct gameState* state = (struct gameState*)malloc(sizeof(struct gameState));
 	state->enemyBullets = 0;
 	state->enemyTanks = 0;
 	state->playerBullets = 0;
-	player->frame = 0;
-	player->xPos = 20;
-	player->yPos = 0;
-	player->speed = 60;
-	player->width = 48;
-	player->direction = 0;
+	state->playerTanks = 0;
 
-	//Create an application window with the following settings:
-	window = SDL_CreateWindow("Battle City",                     // window title
-		SDL_WINDOWPOS_UNDEFINED,           // initial x position
-		SDL_WINDOWPOS_UNDEFINED,           // initial y position
-		15 * BLOCK_X,                               // width, in pixels
-		15 * BLOCK_X,                               // height, in pixels
-		0                                  // flags
-	);
-	(*state).renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
-	loadGame(state);
+	struct Tank* player = (struct Tank*)malloc(sizeof(struct Tank));
+	player->bot = 0;
+	player->bulletPower = 1;
+	player->bulletSpeed = 0;
+	player->direction = 0;
+	player->frame = 0;
+	player->hitPoints = 1;
+	player->lives = 1;
+	player->inAir = 0;
+	player->speed = 24;
+	player->team = 0;
+	player->score = 0;
+	player->upgrade = 0;
+	player->xPos = 10;
+	player->yPos = 10;
+	player->width = 48;
+
+	insertBefore(&(state->playerTanks), player);
+	//state->playerTanks = player;
+	SDL_Event event;
+	SDL_Window *window;
+	SDL_Texture* sprites = NULL;
+	SDL_Renderer* renderer = NULL;//inicijalizacija rendera i tekstura za crtanje
+	int time = 0;//vreme sluzi za animaciju vode
+	SDL_Init(SDL_INIT_VIDEO);
+	window = SDL_CreateWindow("Map Builder", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, BLOCK_X / 4 * 52, BLOCK_X / 4 * 52, 0);//kreiranje prozora
+	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);//kreiranje renderera i biranje prozora u koji renderuje
+	SDL_Surface *surface = NULL;//slika sa koje ce se uzimati teksture
+	surface = IMG_Load("sprites.png");//nece biti ista lokacija fajla vrvtno na kraju
+	sprites = SDL_CreateTextureFromSurface(renderer, surface);//od slike pravi teksturu
+	SDL_FreeSurface(surface);
+	state->renderer = &renderer;
+	state->sprites = &sprites;
+	state->timeStop = 0;
+	generate_random_map(13, 13);
+	state->terrain = read_map(&(state->height), &(state->width), "random_map");
+	state->time = 0;
+
+	struct movementWrapper* wrap = (struct movementWrapper*)malloc(sizeof(struct movementWrapper));
+	wrap->down = 0;
+	wrap->up = 0;
+	wrap->left = 0;
+	wrap->right = 0;
+	wrap->tenkic = player;
 
 	char ggez = 0;
-	read_map(state->terrain, &(state->height), &(state->width), "bmap");
-	struct movementWrapper* wrap = (struct movementWrapper*)malloc(sizeof(struct movementWrapper));
-	(*wrap).down = 0;
-	(*wrap).up = 0;
-	(*wrap).left = 0;
-	(*wrap).right = 0;
-	(*wrap).tenkic = player;
-
 	while (ggez == 0) {
 
-		doRender(state);
+		render_map2(renderer, sprites, state->terrain, state->height, state->width, player, state->time, state->playerBullets->data);
+		//state->time++;
+		//doRender(state);
 		ggez = processEvents(window, wrap);
-		if (wrap->down && wrap->tenkic->direction==2) Move(state, wrap->tenkic, 2);
-		if (wrap->up && wrap->tenkic->direction == 0) Move(state, wrap->tenkic, 0);
-		if (wrap->left && wrap->tenkic->direction == 1) Move(state, wrap->tenkic, 1);
-		if (wrap->right && wrap->tenkic->direction == 3) Move(state, wrap->tenkic, 3);
-
+		time++;
+		if (wrap->up && player->direction == 0) Move(state, player, 0);
+		if (wrap->left && player->direction == 1) Move(state, player, 1);
+		if (wrap->down && player->direction == 2) Move(state, player, 2);
+		if (wrap->right && player->direction == 3) Move(state, player, 3);
 		
-		system("cls");
-		printf("x:%d\ny:%d", wrap->tenkic->xPos, wrap->tenkic->yPos);
-		printf("up %d left %d\ndown %d right %d", wrap->up, wrap->left, wrap->down, wrap->right);
-		
-		SDL_Delay(20);
+		SDL_Delay(1/FPS * 1000);
 	}
 	return 0;
 }
