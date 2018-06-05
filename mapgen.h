0dba009df42d8538db0f6a1300fcd6d61a8d98ee
Tatomirovic <39652686+tatomirovic@users.noreply.github.com>
2018-06-05 22:45:42 +0200

    Add files via upload

--- mapgen.h
+++ mapgen.h
@@ -17,7 +17,6 @@ void generate_path(char** map, int map_h, int map_w);
 char** read_map( int* map_h, int* map_w, char* file_name);
 void generate_tiles(char** map, int map_h, int map_w, int x,int y, int type);
 void set_map_area(char** map, int x, int y, int map_h, int map_w, int type, int big);
-void create_base(char** map, int map_h, int map_w);
 void reset_map(char** map, int map_h, int map_w);
 void print_map(char** map, int map_h, int map_w);
 void deallocate_map(char** map, int map_h);
@@ -28,6 +27,7 @@ int build_map(int map_height, int map_width/*,char** mapx*/);
 void clear_base(char **map, int map_h, int map_w);
 int random(int max);
 int generate_random_map(int map_height, int map_width);
+void print_map_file(char** map, int map_h, int map_w, FILE* fmap);
 
 
 
