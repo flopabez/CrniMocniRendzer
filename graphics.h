7b4878063485c305064cd1110af4b9d93d32bef3
DamjanTM <39652602+DamjanTM@users.noreply.github.com>
2018-06-07 01:06:28 +0200

    Update graphics.h

--- graphics.h
+++ graphics.h
@@ -3,6 +3,8 @@
 
 #define PLAYER_SPEED 3
 #define BLOCK_X 48
+#define WINDOW_W BLOCK_X * (16+2)
+#define WINDOW_H BLOCK_X * 16
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -14,5 +16,20 @@ void loadGame(struct gameState *gameState);
 
 void doRender(struct gameState *gameState, SDL_Renderer *renderer, SDL_Texture *sprites);
 
-
 #endif /* graphics_h */
+
+/*typedef struct explosion {
+	int xPos, yPos;
+	char size;
+	int time;
+} Explosion;
+
+
+void Boom(int xPos, int yPos, char size, struct listNode* lista_ekspolzija, struct gameState* gameState) {
+	Explosion *temp = malloc(sizeof(Explosion));
+	temp->xPos = xPos;
+	temp->yPos = yPos;
+	temp->size = size;
+	temp->time = 0;
+	insertBefore();
+}*/
