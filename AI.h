73ebbdb17beb34d1f03233a725921fe205e881a7
pseto-kaiba <39837275+pseto-kaiba@users.noreply.github.com>
2018-06-07 14:54:45 +0200

    Update AI.h

--- AI.h
+++ AI.h
@@ -1,6 +1,24 @@
 #pragma once
 #include "strukture.h"
 
+#define SEE_DIST 30
+#define BRICK 1
+#define METAL 2
+#define WATER 3
+#define ICE 5
+#define FOREST 4
+#define BASE 6
+#define EMPTY 0
+#define BLOCK_X 48
+
+#define UP 0
+#define LEFT 1
+#define DOWN 2
+#define RIGHT 3
+#define SHOOT 4
+#define SIT 5
+
+
 typedef struct Tank Tank;
 typedef struct listNode listNode;
 typedef struct gameState gameState;
