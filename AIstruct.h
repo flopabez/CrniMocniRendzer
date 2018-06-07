7a9856543a39d8fe51ea2c23ea0326da462c8947
pseto-kaiba <39837275+pseto-kaiba@users.noreply.github.com>
2018-06-07 15:15:58 +0200

    Update AIstruct.h

--- AIstruct.h
+++ AIstruct.h
@@ -1,4 +1,20 @@
 #pragma once
+#define SEE_DIST 30
+#define BRICK 1
+#define METAL 2
+#define WATER 3
+#define ICE 5
+#define FOREST 4
+#define BASE 6
+#define EMPTY 0
+#define BLOCK_X 48
+
+#define UP 0
+#define LEFT 1
+#define DOWN 2
+#define RIGHT 3
+#define SHOOT 4
+#define SIT 5
 
 typedef struct tankMoves
 {
