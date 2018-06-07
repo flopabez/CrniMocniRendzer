ba6a399ae504681f433eb93dfed73dd4aafe804e
DamjanTM <39652602+DamjanTM@users.noreply.github.com>
2018-06-07 20:37:54 +0200

    Update graphics.c

--- graphics.c
+++ graphics.c
@@ -6,15 +6,15 @@
 
 void doRender(struct gameState *gameState, SDL_Renderer *renderer, SDL_Texture *sprites)
 {
-	int xofs = (WINDOW_H-gameState->width/4*BLOCK_X)/2;
-	int yofs = (WINDOW_H-gameState->height/4*BLOCK_X)/2;
+	int xofs = (WINDOW_H - gameState->width / 4 * BLOCK_X) / 2;
+	int yofs = (WINDOW_H - gameState->height / 4 * BLOCK_X) / 2;
 	/*
 	stavi ovo u struct gameState:
-		int xoffset, yoffset;
+	int xoffset, yoffset;
 
 	stavi ovo u main:
-		state->yoffset = (WINDOW_H-state->height/4*BLOCK_X)/2;
-		state->xoffset = (WINDOW_H -state->width / 4 * BLOCK_X) / 2;
+	state->yoffset = (WINDOW_H-state->height/4*BLOCK_X)/2;
+	state->xoffset = (WINDOW_H -state->width / 4 * BLOCK_X) / 2;
 	*/
 
 
@@ -27,29 +27,29 @@ void doRender(struct gameState *gameState, SDL_Renderer *renderer, SDL_Texture *
 
 	//Draw map background
 	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
-	SDL_Rect bg = { xofs , yofs , gameState->width/4*BLOCK_X, gameState->height/4*BLOCK_X };
+	SDL_Rect bg = { xofs , yofs , gameState->width / 4 * BLOCK_X, gameState->height / 4 * BLOCK_X };
 	SDL_RenderFillRect(renderer, &bg);
 
 	//Draw GUI///////////////////////////////////////////////////////////////////
-	int xGUI = ((xofs + gameState->width / 4 * BLOCK_X) + WINDOW_W - BLOCK_X)/2;
+	int xGUI = ((xofs + gameState->width / 4 * BLOCK_X) + WINDOW_W - BLOCK_X) / 2;
 	int yGUI = (WINDOW_H - BLOCK_X * 10.5) / 2;
-	SDL_Rect tank_count_loc = { xGUI, yGUI, BLOCK_X, BLOCK_X*5 };
-	SDL_Rect tank_count_sloc = {376, 24, 16, 80};
+	SDL_Rect tank_count_loc = { xGUI, yGUI, BLOCK_X, BLOCK_X * 5 };
+	SDL_Rect tank_count_sloc = { 376, 24, 16, 80 };
 	SDL_RenderCopy(renderer, sprites, &tank_count_sloc, &tank_count_loc);
 
 	for (int i = 0; i < 20; i++) {
-		SDL_Rect tank_mark_loc = { xGUI+(BLOCK_X/2)*(i%2), yGUI + (i/2)*(BLOCK_X/2), BLOCK_X/2, BLOCK_X/2 };
-		SDL_Rect tank_mark_sloc = { 328+8*1/*(gameState->tanksleft>i)*/, 192, 8, 8 };
+		SDL_Rect tank_mark_loc = { xGUI + (BLOCK_X / 2)*(i % 2), yGUI + (i / 2)*(BLOCK_X / 2), BLOCK_X / 2, BLOCK_X / 2 };
+		SDL_Rect tank_mark_sloc = { 328 + 8 * 1/*(gameState->tanksleft>i)*/, 192, 8, 8 };
 		SDL_RenderCopy(renderer, sprites, &tank_mark_sloc, &tank_mark_loc);
 	}
 
-	SDL_Rect P1_loc = { xGUI, yGUI+BLOCK_X*6, BLOCK_X, BLOCK_X };
+	SDL_Rect P1_loc = { xGUI, yGUI + BLOCK_X * 6, BLOCK_X, BLOCK_X };
 	SDL_Rect P1_sloc = { 376, 136, 16, 16 };
 	SDL_RenderCopy(renderer, sprites, &P1_sloc, &P1_loc);
 
 	struct Tank *p1 = (struct Tank*)gameState->playerTanks->data;
 
-	SDL_Rect num_loc = { xGUI+ BLOCK_X/2, yGUI + BLOCK_X * 6 + BLOCK_X /2, BLOCK_X/2, BLOCK_X/2 };
+	SDL_Rect num_loc = { xGUI + BLOCK_X / 2, yGUI + BLOCK_X * 6 + BLOCK_X / 2, BLOCK_X / 2, BLOCK_X / 2 };
 	SDL_Rect num_sloc = { 289 + 8 * (p1->lives), 200, 8, 8 };
 	SDL_RenderCopy(renderer, sprites, &num_sloc, &num_loc);
 
@@ -81,8 +81,8 @@ void doRender(struct gameState *gameState, SDL_Renderer *renderer, SDL_Texture *
 	for (int i = 0; i<gameState->height; i++) {
 		for (int j = 0; j<gameState->width; j++) {
 			if (!gameState->terrain[i][j] || gameState->terrain[i][j] == 4) continue;
-			SDL_Rect location = { xofs+j*BLOCK_X / 4, yofs + i*BLOCK_X / 4, BLOCK_X / 4, BLOCK_X / 4 };
-			SDL_Rect block = { 256 + ((j % 4) * 16 / 4) + (gameState->terrain[i][j] == 3)*((gameState->time % 90) / 15) * 16, (gameState->terrain[i][j] - 1) * 16 + ((i % 4) * 16 / 4), 16 / 4, 16 / 4 };
+			SDL_Rect location = { xofs + j * BLOCK_X / 4, yofs + i * BLOCK_X / 4, BLOCK_X / 4, BLOCK_X / 4 };
+			SDL_Rect block = { 256 + ((j % 4) * 16 / 4) + (gameState->terrain[i][j] == 3)*((gameState->time % 18) / 3) * 16, (gameState->terrain[i][j] - 1) * 16 + ((i % 4) * 16 / 4), 16 / 4, 16 / 4 };
 			SDL_RenderCopy(renderer, sprites, &block, &location);
 		}
 	}
@@ -104,6 +104,7 @@ void doRender(struct gameState *gameState, SDL_Renderer *renderer, SDL_Texture *
 
 	while (etank_wrapper->data) {
 		struct Tank *enemy = (struct Tank*)etank_wrapper->data;
+		if (!(gameState->time % 2)) enemy->frame = (enemy->frame + 1) % 2;
 		SDL_Rect rect = { xofs + enemy->xPos, yofs + enemy->yPos, BLOCK_X, BLOCK_X };
 		SDL_Rect dest = { 0 + enemy->frame * 16 + enemy->direction * 16 * 2, 0 + enemy->upgrade * 16, 16, 16 };
 		SDL_RenderCopy(renderer, sprites, &dest, &rect);
@@ -136,8 +137,8 @@ void doRender(struct gameState *gameState, SDL_Renderer *renderer, SDL_Texture *
 	for (int i = 0; i<gameState->height; i++) {
 		for (int j = 0; j<gameState->width; j++) {
 			if (gameState->terrain[i][j] != 4) continue;
-			SDL_Rect location = { xofs + j*BLOCK_X / 4,yofs + i*BLOCK_X / 4, BLOCK_X / 4, BLOCK_X / 4 };
-			SDL_Rect block = { 256 + ((j % 4) * 16 / 4) + ((gameState->time % 600) / 150) * 16, (gameState->terrain[i][j] - 1) * 16 + ((i % 4) * 16 / 4), 16 / 4, 16 / 4 };
+			SDL_Rect location = { xofs + j * BLOCK_X / 4,yofs + i * BLOCK_X / 4, BLOCK_X / 4, BLOCK_X / 4 };
+			SDL_Rect block = { 256 + ((j % 4) * 16 / 4) + ((gameState->time % 120) / 30) * 16, (gameState->terrain[i][j] - 1) * 16 + ((i % 4) * 16 / 4), 16 / 4, 16 / 4 };
 			SDL_RenderCopy(renderer, sprites, &block, &location);
 		}
 	}
@@ -147,14 +148,14 @@ void doRender(struct gameState *gameState, SDL_Renderer *renderer, SDL_Texture *
 	// a xPos i yPos vec znate
 	/*/Draw upgrade
 	if (gameState->pow.num) {
-		SDL_Rect rect = { xofs+gameState->pow.xPos, yofs+gameState->pow.yPos, BLOCK_X, BLOCK_X };
-		SDL_Rect dest = { 256 + (gameState->pow.num-1)*16, 112, 16, 16};
-		SDL_RenderCopy(renderer, sprites, &dest, &rect);
+	SDL_Rect rect = { xofs+gameState->pow.xPos, yofs+gameState->pow.yPos, BLOCK_X, BLOCK_X };
+	SDL_Rect dest = { 256 + (gameState->pow.num-1)*16, 112, 16, 16};
+	SDL_RenderCopy(renderer, sprites, &dest, &rect);
 	}
 	//*/
 
 	/* Pretpostavio sam da eksplozija izgleda ovako i da se nalaze u listi unutar gameState-a
-	
+
 	typedef struct explosion {
 	int xPos, yPos;
 	char size;
@@ -163,15 +164,15 @@ void doRender(struct gameState *gameState, SDL_Renderer *renderer, SDL_Texture *
 
 	*/
 
-	/*/Draw explosions
+	//Draw explosions
 	struct listNode *booms = gameState->explosions;
 	while (booms->data) {
-		struct Explosion *exp = (struct Bullet*)booms->data;
+		Explosion *exp = (struct Bullet*)booms->data;
 		SDL_Rect rect = { xofs+exp->xPos - (BLOCK_X/2*(exp->size + 1)), yofs+exp->yPos - (BLOCK_X / 2 * (exp->size + 1)), BLOCK_X*(exp->size + 1), BLOCK_X*(exp->size + 1) };
 		SDL_Rect dest = { 256 + exp->size*48 + 16*(exp->time / 15)*(exp->size+1), 128, 16*(exp->size+1), 16*(exp->size + 1) };
 		SDL_RenderCopy(renderer, sprites, &dest, &rect);
 		exp->time++;
-		if ((exp->time = 45 && exp->size = 0) || (exp->time = 30 && exp->size = 1)) removeNode(&booms);
+		if ((exp->time == 45 && exp->size == 0) || (exp->time == 30 && exp->size == 1)) removeNode(&booms);
 		else booms = booms->next;
 	}
 	//*/
