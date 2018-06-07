31dd411c4fd9e00c11bae8ef041c16409768b3fe
flopabez <33830264+flopabez@users.noreply.github.com>
2018-06-07 13:44:30 +0200

    Update hook.h

--- hook.h
+++ hook.h
@@ -1,6 +1,6 @@
 #ifndef HOOK_H
 #define HOOK_H
 
-int processEvents(SDL_Window *window, struct movementWrapper *wrap);
+int processEvents(SDL_Window *window, struct movementWrapper *wrap, struct gameState* state);
 
 #endif
