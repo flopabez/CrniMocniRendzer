5de797e520183f52ed6a1402ea47d5c736f5d065
DamjanTM <39652602+DamjanTM@users.noreply.github.com>
2018-06-07 21:08:49 +0200

    Update graphics.h

--- graphics.h
+++ graphics.h
@@ -17,19 +17,3 @@ void loadGame(struct gameState *gameState);
 void doRender(struct gameState *gameState, SDL_Renderer *renderer, SDL_Texture *sprites);
 
 #endif /* graphics_h */
-
-/*typedef struct explosion {
-	int xPos, yPos;
-	char size;
-	int time;
-} Explosion; 
-
-
-void Boom(int xPos, int yPos, char size, struct listNode* lista_ekspolzija, struct gameState* gameState) {
-	Explosion *temp = malloc(sizeof(Explosion));
-	temp->xPos = xPos;
-	temp->yPos = yPos;
-	temp->size = size;
-	temp->time = 0;
-	insertBefore();
-}*/
