9ef084eb5a04025f4b4681637ca4f56d2b7a7a47
DamjanTM <39652602+DamjanTM@users.noreply.github.com>
2018-06-08 02:38:57 +0200

    Update graphics.c

--- graphics.c
+++ graphics.c
@@ -96,7 +96,7 @@ void doRender(struct gameState *gameState, SDL_Renderer *renderer, SDL_Texture *
 		SDL_Rect dest = { 0 + player->frame * 16 + player->direction * 16 * 2, 0 + (playerNum * 128) + player->upgrade * 16, 16, 16 };
 		SDL_RenderCopy(renderer, sprites, &dest, &rect);
 		if (player->shield) {
-			SDL_Rect shield_sprite = { 256 * (gameState->time%2)*16, 144, 16, 16 };
+			SDL_Rect shield_sprite = { 256 + (gameState->time%2)*16, 144, 16, 16 };
 			SDL_RenderCopy(renderer, sprites, &shield_sprite, &rect);
 		}
 
@@ -152,7 +152,7 @@ void doRender(struct gameState *gameState, SDL_Renderer *renderer, SDL_Texture *
 		}
 	}
 
-	/*/Draw pickup
+	//Draw pickup
 	if (gameState->pickup) {
 	SDL_Rect rect = { xofs+ gameState->pickup->xPos, yofs+ gameState->pickup->yPos, BLOCK_X, BLOCK_X };
 	SDL_Rect dest = { 256 + (gameState->pickup->type)*16, 112, 16, 16};
