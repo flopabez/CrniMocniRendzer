26b9473813fbefdc3b4b93bf73c850fa103a32e8
flopabez <33830264+flopabez@users.noreply.github.com>
2018-06-08 08:51:46 +0200

    Update loop.c

--- loop.c
+++ loop.c
@@ -13,22 +13,6 @@
 
 int main() {
 
-	struct gameState* state = (struct gameState*)malloc(sizeof(struct gameState));
-	state->explosions = newNode(0);
-	state->enemyBullets = newNode(0);
-	state->enemyTanks = newNode(0);
-	state->playerBullets = newNode(0);
-	state->playerTanks = newNode(0);
-	state->timeStop = 0;
-	generate_random_map(13, 13);
-	state->terrain = read_map(&(state->height), &(state->width), "random_map");
-	state->time = 0;
-	state->pickup = 0;
-	state->shovel = 0;
-	state->dif = 2;
-	state->killCount = 20;
-	state->stage = 0;
-	
 
 	SDL_Event event;
 	SDL_Window *window;
@@ -41,8 +25,37 @@ int main() {
 	surface = IMG_Load("sprites.png");//nece biti ista lokacija fajla vrvtno na kraju
 	sprites = SDL_CreateTextureFromSurface(renderer, surface);//od slike pravi teksturu
 	SDL_FreeSurface(surface);
+	if (!sprites) printf("Can't find file 'sprites.png'");
 
-	if(!sprites) printf("Can't find file 'sprites.png'");
+	srand(time(NULL));
+	struct gameState* state = 0;
+	
+
+	char ggez = 0;
+	
+	while (ggez != 1) {
+		ggez = doMenu(window, renderer, sprites, 0);
+		switch (ggez) {
+		case 1:;
+			printf("%d", ggez);
+			state = initGame(0);
+			printf("%d", ggez);
+			break;
+		case 3: break;
+		case 4:;
+			int a, b;
+			printf("visina i sirina mape: ");
+			scanf("%d%d", &a, &b);
+			build_map(a, b);
+			break;
+		case 5: break;
+		case 6:;
+			return 0;
+			break;
+		}
+	}
+	
+	ggez = 0;
 
 	struct Tank* player = spawnTank(state, 0, 2, 0);
 
@@ -53,12 +66,10 @@ int main() {
 	wrap->right = 0;
 	wrap->tenkic = player;
 
-	doMenu(window, renderer, sprites);
-	char ggez = 0;
+
 	float delay = 1. / FPS * 1000;
-	struct Tank* bot = spawnTank(state, 1, 0, 1);
-	bot->pickup = 1;
-	state->timeStop = 10 * 24;
+	int spawnDelay = 24 * (4 - state->dif);
+	char maxOnscreen = 3 + state->dif;
 
 	while (ggez == 0) {
 
@@ -67,6 +78,43 @@ int main() {
 
 
 		ggez = processEvents(window, wrap, state);
+		if (ggez == -1) {
+			while (ggez != 1) {
+				ggez = doMenu(window, renderer, sprites, 1);
+				switch (ggez) {
+				case 1:;
+					free(state);
+					state = initGame(0);
+					free(player);
+					player = spawnTank(state, 0, 2, 0);
+					free(wrap);
+					wrap = (struct movementWrapper*)malloc(sizeof(struct movementWrapper));
+					wrap->down = 0;
+					wrap->up = 0;
+					wrap->left = 0;
+					wrap->right = 0;
+					wrap->tenkic = player;
+					break;
+				case 2:;
+					ggez = 1;
+					break;
+				case 3: break;
+				case 4:;
+					int a, b;
+					printf("visina i sirina mape: ");
+					scanf("%d%d", &a, &b);
+					build_map(a, b);
+					break;
+				case 5: break;
+				case 6:;
+					free(state);
+					return 0;
+					break;
+				}
+			}
+			ggez = 0;
+		}
+
 		if (wrap->up && player->direction == 0) Move(state, player, 0);
 		if (wrap->left && player->direction == 1) Move(state, player, 1);
 		if (wrap->down && player->direction == 2) Move(state, player, 2);
@@ -80,11 +128,13 @@ int main() {
 		if (state->timeStop == 0) updateBots(state);
 		doRender(state, renderer, sprites); // updateovanje gamestatea
 	
-
+		
+		if ( !(state->killCount == 0 || spawnDelay || botCount(state) > maxOnscreen) ) spawnTank(state, 1 + random(4), random(2), 1);
+		if (spawnDelay == 0) spawnDelay = 24 * (4 - state->dif);
+		spawnDelay--; //spawnovanje botova
 
 		state->time++;
 		SDL_Delay(delay);
-
 	}
 	return 0;
 }
